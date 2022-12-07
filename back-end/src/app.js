d5fcc8bd7b3342351c0f040ce4d127a1d5b821d9
Keri Wilkins <keriannwilkins@gmail.com>
2022-12-07 13:46:50 -0600

    setting up all basic files in backend

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -4,17 +4,24 @@ require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
 const express = require("express");
 const cors = require("cors");
+const knex = require("./db/connection");
 
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
+
 const reservationsRouter = require("./reservations/reservations.router");
+const tablesRouter = require("./tables/tables.router");
 
 const app = express();
 
+app.set("db", knex);
 app.use(cors());
 app.use(express.json());
+// pre-flight cors checks 
+app.options('*', cors());
 
 app.use("/reservations", reservationsRouter);
+app.use("/tables", tablesRouter);
 
 app.use(notFound);
 app.use(errorHandler);
