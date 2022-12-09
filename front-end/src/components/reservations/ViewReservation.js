5d629a1e044af4a61b13f3a7e5a7c4cc5c1bfa48
Keri Wilkins <keriannwilkins@gmail.com>
2022-12-09 11:05:41 -0600

    working on getting the results from viewReservations from the search page

--- front-end/src/components/reservations/ViewReservation.js
+++ front-end/src/components/reservations/ViewReservation.js
@@ -27,38 +27,58 @@ const ViewReservation = ({ reservation }) => {
 
   return (
     <div>
-      <p data-reservation-id-status={reservation.reservation_id}>{reservation.status}</p>
-      <p>Name: {reservation.first_name} {reservation.last_name}</p>
-      <p>Mobile: {reservation.mobile_number}</p>
-      <p>Party Size: {reservation.people}</p>
-      <p>Reservation on: {reservation.reservation_date} at {reservation.reservation_time}</p>
-
-      <div>
-        <ErrorAlert error={showError} />
-        {reservation.status === "booked" ? (
-          <button className="btn btn-success my-3 mr-3 px-3 py-2">
+    <table className="table table-hover">
+      <thead className="thead-light">
+        <tr>
+          <th scope="col">Status</th>
+          <th scope="col">Full Name</th>
+          <th scope="col">Phone Number</th>
+          <th scope="col">People</th>
+          <th scope="col">Date</th>
+          <th scope="col">Time</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">{reservation.status}</th>
+          <th style={{width: "20%"}}>{reservation.first_name} {reservation.last_name}</th>
+          <th>{reservation.mobile_number}</th>
+          <th>{reservation.people}</th>
+          <th>{reservation.reservation_date}</th>
+          <th>{reservation.reservation_time}</th>
+        </tr>
+        <div>
+          <tr>
+            <th>
+          <ErrorAlert error={showError} />
+          {reservation.status === "booked" ? (
+            <button className="btn btn-success my-3 mr-3 px-3 py-2">
+              <a
+                href={`/reservations/${reservation.reservation_id}/seat`}
+                style={{ color: "white", textDecoration: "none" }}
+              >Seat
+              </a>
+            </button>
+          ) : null}</th><th>
+          <button className="btn btn-warning px-3 py-2">
             <a
-              href={`/reservations/${reservation.reservation_id}/seat`}
+              href={`/reservations/${reservation.reservation_id}/edit`}
               style={{ color: "white", textDecoration: "none" }}
-            >Seat
+            >Edit
             </a>
-          </button>
-        ) : null}
-        <button className="btn btn-warning px-3 py-2">
-          <a
-            href={`/reservations/${reservation.reservation_id}/edit`}
-            style={{ color: "white", textDecoration: "none" }}
-          >Edit
-          </a>
-        </button>
-        <button
-          className="btn btn-secondary mx-3 px-3 py-2"
-          data-reservation-id-cancel={reservation.reservation_id}
-          onClick={handleCancel}
-        > Cancel
-        </button>
-      </div>
-    </div>
+          </button></th><th>
+          <button
+            className="btn btn-secondary mx-3 px-3 py-2"
+            data-reservation-id-cancel={reservation.reservation_id}
+            onClick={handleCancel}
+          > Cancel
+          </button></th>
+          </tr>
+        </div>
+      </tbody>
+    </table>
+     
+    </div> 
   );
 };
 
--- front-end/src/components/search/Search.js
+++ front-end/src/components/search/Search.js
@@ -3,66 +3,84 @@ import ErrorAlert from "../../layout/ErrorAlert";
 import { listReservations } from "../../utils/api";
 import ViewReservation from "../reservations/ViewReservation";
 
-
 const Search = () => {
   //image
-const externalImage = "https://worldarchitecture.org/cdnimgfiles/extuploadc/15xxzwluzwil9549.jpg"
-const initialState = { mobile_number: "--- --- ----"}
-    
-    //useStates
+  const externalImage =
+    "https://worldarchitecture.org/cdnimgfiles/extuploadc/15xxzwluzwil9549.jpg";
+  const initialState = { mobile_number: "--- --- ----" };
+
+  //useStates
   const [reservations, setReservations] = useState([]);
   const [formData, setFormData] = useState({ ...initialState });
   const [showError, setShowError] = useState(null);
 
-// handle change
-function handleChange({ target }) {
+  // handle change
+  function handleChange({ target }) {
     setFormData({
-        ...formData,
-        [target.name]: target.value,
+      ...formData,
+      [target.name]: target.value,
     });
-};
+  }
 
-//handle submit
-async function handleSubmit(event) {
+  //handle submit
+  async function handleSubmit(event) {
     event.preventDefault();
     const abortController = new AbortController();
     const searchQuery = {
-        mobile_number: formData.mobile_number,
+      mobile_number: formData.mobile_number,
     };
     setFormData(initialState);
     setShowError(null);
     try {
-        const getData = await listReservations(searchQuery, abortController.signal)
-        setReservations(getData);
+      const getData = await listReservations(
+        searchQuery,
+        abortController.signal
+      );
+      setReservations(getData);
     } catch (error) {
-        if (error.name !== "AbortError") setShowError(error);
+      if (error.name !== "AbortError") setShowError(error);
     }
     return () => abortController.abort();
-}
+  }
 
-const searchResults =
+  const searchResults =
     reservations.length > 0
       ? reservations.map((reservation) => (
           <ViewReservation
             key={reservation.reservation_id}
             reservation={reservation}
           />
-        )) : null;
+        ))
+      : null;
 
   return (
-    <main style={{
-      backgroundImage: `url(${externalImage})`,
-      backgroundSize: "cover",
-      backgroundRepeat: "no-repeat",
-      backgroundPosition: "center",
-      filter: "brightness(0.8)" + "brightness(1.3)",
-      padding: "85px", 
-      borderRadius: "15px",
-      }}>
-      <div className="d-md-flex mb-3" style={{ justifyContent: "center", alignItems: "center"}}>
-          <h3 style={{color: "white"}}>Search for a reservation by phone number</h3>
-          </div>
-          <div style={{ display: "flex", justifyContent: "center", alignItems: "center"}}>
+    <main>
+      <div
+        style={{
+          backgroundImage: `url(${externalImage})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          filter: "brightness(0.8)" + "brightness(1.3)",
+          padding: "85px",
+          borderRadius: "15px",
+        }}
+      >
+        <div
+          className="d-md-flex mb-3"
+          style={{ justifyContent: "center", alignItems: "center" }}
+        >
+          <h3 style={{ color: "white" }}>
+            Search for a reservation by phone number
+          </h3>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           <form onSubmit={handleSubmit}>
             <input
               id="mobile_number"
@@ -72,19 +90,19 @@ const searchResults =
               value={FormData.mobile_phone}
               placeholder="(---) --- ----"
               required
-              />
+            />
             &nbsp;
             <button type="submit" className="btn btn-dark">
               Find Reservation
             </button>
           </form>
-        <div>
-          {searchResults}
         </div>
-      
-        <ErrorAlert error={showError} />
+      </div>
+      <div>
+        <div className="card">{searchResults}</div>
+      </div>
 
-          </div>
+      <ErrorAlert error={showError} />
     </main>
   );
 };
